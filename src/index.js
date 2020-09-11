f6375a8b38f557f329aa66949af3c394c6c68d06
Louis Kenyon <louis.kenyon@kenyoncomputing.co.uk>
2020-09-11 18:38:59 +0100

    develop - Rename MapLoad component to Map

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
-import MapLoad from './MapLoad';
+import Map from './Map/Map';
 
 const root = document.getElementById("root");
-render(<MapLoad/>, root);
+render(<Map/>, root);
