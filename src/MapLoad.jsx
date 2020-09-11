c9fdfdb948f7d2e4e05f58dcefef310ac89fbd3f
Louis Kenyon <louis.kenyon@kenyoncomputing.co.uk>
2020-09-11 18:31:47 +0100

    develop - Update map to use refs, move css to new file, and remove unneeded state updates

--- src/MapLoad.jsx
+++ src/MapLoad.jsx
@@ -1,43 +1,43 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import mapboxgl from 'mapbox-gl';
 import './MapLoad.css';
 
 mapboxgl.accessToken = "pk.eyJ1Ijoia2VueW9ubCIsImEiOiJja2V3dml4c3gwNTN2MnluMXVqZnh2dG96In0.Rf1jMcyVc831BWikuxhB9g";
 
-const MapLoad = (props) => {
-  const [mapData, setMapData] = useState({});
+const MapLoad = () => {
+  const [mapData, setMapData] = useState({
+    lng: 5,
+    lat: 34,
+    zoom: 2
+  })
+  const mapContainerRef = useRef(null);
   useEffect(() => {
-    if (!Object.keys(mapData).length) {
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: 'mapbox://styles/kenyonl/ckewvssax151z1ap9u8gh6geq',
+      center: [mapData.lng, mapData.lat],
+      zoom: mapData.zoom
+    });
+
+    map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+
+    map.on('move', () => {
       setMapData({
-        lng: 5,
-        lat: 34,
-        zoom: 2
-      })
-    } else {
-      const map = new mapboxgl.Map({
-        container: document.getElementById("mapContainer"),
-        style: 'mapbox://styles/kenyonl/ckewvssax151z1ap9u8gh6geq',
-        center: [mapData.lng, mapData.lat],
-        zoom: mapData.zoom
+        lng: map.getCenter().lng.toFixed(4),
+        lat: map.getCenter().lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2)
       });
-      map.on('move', () => {
-        setMapData({
-          lng: map.getCenter().lng.toFixed(4),
-          lat: map.getCenter().lat.toFixed(4),
-          zoom: map.getZoom().toFixed(2)
-        });
-      })
-    }
-
-
-  }, [mapData])
+    })
+    return () => map.remove();
+  }, [])
   return (
     <div>
-      <div>
-        <div className='sidebarStyle'>Longitude: {mapData.lng} | Latitude: {mapData.lat} | Zoom: {mapData.zoom}</div>
+      <div className='sidebarStyle'>
+        <div>
+          Longitude: {mapData.lng} | Latitude: {mapData.lat} | Zoom: {mapData.zoom}
+        </div>
       </div>
-      <div style={{position: "absolute", top: 0, right: 0, left: 0, bottom: 0}} id="mapContainer"
-           className="mapContainer"/>
+      <div className="mapContainer" ref={mapContainerRef}/>
     </div>
   )
 }
