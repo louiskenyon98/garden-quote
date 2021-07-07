905b0a7819bb4bb18e190fe13d00eafe22c1cca1
Louis Kenyon <louis.kenyon@kenyoncomputing.co.uk>
2021-07-07 18:27:10 +0100

    Render google maps component instead of mapbox

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 import Map from './Map/Map';
+import GMap from './Map/GMap'
 
 const root = document.getElementById("root");
-render(<Map/>, root);
+render(<GMap/>, root);
